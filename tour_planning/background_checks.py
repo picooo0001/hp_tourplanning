af77e798154fcaedef6d30136bddb0b1421000b0
pico_hp <nicopeuser5@gmail.com>
2023-11-16 00:03:34 +0100

    Klasse BackgroundChecks überarbeit. Funktioniert im Zusammenspiel mit TourCreation und checkt die Adresse zuverlässig. Außerdem loggt sie den Input, sowie den Output der Api

--- tour_planning/background_checks.py
+++ tour_planning/background_checks.py
@@ -1,27 +1,52 @@
 import requests
+import logging
+from datetime import *
 
 class BackgroundChecks:
     """
-    Diese Klasse überprüft im Hintergrund on die eingegebenen Werte stimmen können.
+    Initialisiert einen AdresseValidator mit den angegebenen Adressinformationen.
 
     Args:
-        strasse (str): Der zu überprüfende Straßenname
-    
-    Returns:
-        bool: True, wenn die Straße existiert, sonst False
+        strasse (str): Straßenname der Adresse.
+        hausnr (str): Hausnummer der Adresse.
+        plz (str): Postleitzahl der Adresse.
+        ort (str): Ort der Adresse.
     """
-    @staticmethod
-    def check_userinput_strasse(strasse):
-        
-        url = 'https://nominatim.openstreetmap.org/search'
-        parameter = {
-            "format": "json",
-            "street": strasse,
-            "limit": 3
+    def __init__(self, strasse, hausnr, plz, ort):
+        self.strasse = strasse
+        self.hausnr = hausnr
+        self.plz = plz
+        self.ort = ort
+
+        logging.basicConfig(filename='api_logs.log', level=logging.INFO)
+
+    def check_adress_existance(self):
+        """
+        Überprüft die Existenz der angegebenen Adresse mithilfe der OpenStreetMap Nominatim API.
+
+        Returns:
+            bool: True, wenn die Adresse existiert; False, wenn die Adresse nicht existiert.
+        """
+        address = f"{self.strasse} {self.hausnr} {self.plz} {self.ort}"
+
+        url = "https://nominatim.openstreetmap.org/search"
+
+        params = {
+            'format': 'json',
+            'q': address
         }
 
-        response = requests.get(url, params=parameter)
-        data = response.json
+        response = requests.get(url, params=params)
 
-        print("Inhalt von response.json():", response.json())
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_message = f"[{timestamp}] Adresse: {address}, API-Antwort: {response.json()}, API-Statuscode: {response.status_code}"
+        logging.info(log_message)
+
+        if response.ok and len(response.json()) > 0:
+            return True
+        else:
+            return False
+
+
+        
 

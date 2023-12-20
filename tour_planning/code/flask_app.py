130db9868d2cc36c7ef41229bb8b80055cb639bf
Nico Peuser <nicopeuser@a146d8bd-42d1-4840-965f-ceb40f03671e.fritz.box>
2023-12-20 18:57:01 +0100

    Got get_tours methode route fully working. Now displaying calendar entries from DB

--- tour_planning/code/flask_app.py
+++ tour_planning/code/flask_app.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, render_template, jsonify
 from input_to_database import DataWriter
+from orm import Tour, Address, Client
 from db_connect_disconnect import DatabaseConnector
+from datetime import datetime
 
-from orm import Tour  # Import your Tour model
-from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 
+db_connection = DatabaseConnector('postgresql://hp_admin:Nudelholz03#@localhost/hp_postgres')
+
 @app.route('/')
 def index():
-    return render_template('tourcreation.html')
+    return render_template('tours.html')
 
 @app.route('/create_tour', methods=['POST'])
 def create_tour():
@@ -35,29 +38,47 @@ def create_tour():
             return jsonify({'message': 'Tour successfully created!'})
         else:
             return jsonify({'message': 'Error creating tour!'}), 500
-    
-@app.route('/tours')
-def display_tours():
+
+
+@app.route('/get_tours', methods=['GET'])
+def get_tours():
     try:
-        # Create an engine to connect to your database
-        engine = create_engine('postgresql://hp_admin:Nudelholz03#@localhost/hp_postgres')
+        start_date_str = request.args.get('start')  # Nur das Startdatum als String aus der URL abrufen
+        end_date_str = request.args.get('end')      # Nur das Enddatum als String aus der URL abrufen
 
-        # Create a session to interact with the database
-        Session = sessionmaker(bind=engine)
-        session = Session()
+        # Konvertiere das Start- und Enddatum von String zu DateTime
+        start_date = datetime.fromisoformat(start_date_str.replace('T', ' '))
+        end_date = datetime.fromisoformat(end_date_str.replace('T', ' '))
 
-        # Query the Tour table to fetch all tour data
-        tours = session.query(Tour).all()
+        # Sitzung für Datenbankoperationen erhalten
+        db_session, connection_status = db_connection.get_session()
 
-        # Close the session
-        session.close()
+        # Daten aus der Datenbank abrufen und formatieren basierend auf dem Enddatum
+        events = db_session.query(Tour, Address, Client)\
+            .join(Client, Client.client_id == Tour.client_id)\
+            .join(Address, Address.address_id == Tour.address_id)\
+            .filter(Tour.date >= start_date, Tour.date <= end_date).all()
 
-        # Render a template and pass the fetched tour data to it
-        return render_template('tours.html', tours=tours)
+        formatted_tours = []
+        for tour, address, client in events:
+            formatted_tours.append({
+                'title': f"{tour.kolonne_type} - {client.firmenname}",
+                'start': tour.date.isoformat(),  # Datumsformat anpassen
+                'description': f"Address: {address.strasse} {address.hausnr}, {address.plz} {address.ort}. Further Info: {tour.further_info}"
+            })
 
-    except Exception as e:
-        return render_template('error.html', error=str(e))
+        # Verbindung zur Datenbank schließen
+        close_status, close_message = db_connection.close_connection()
+        if not close_status:
+            print("Fehler beim Schließen der Verbindung:", close_message)
+
+        # Tour-Daten als JSON zurückgeben
+        return jsonify(formatted_tours)
     
+    except Exception as e:
+        print("Fehler:", str(e))  # Hier kannst du den Fehler in der Konsole anzeigen
+        return jsonify({'error': str(e)}), 500
 
+    
 if __name__ == '__main__':
     app.run(debug=True)
